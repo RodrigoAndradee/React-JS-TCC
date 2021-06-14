bd290981865a21905377c8e0a85382fdd465803e
gabriel <gabriel.talles00@gmail.com>
2021-06-14 19:36:24 -0300

    initial grafic

--- src/pages/homeScreen/HomeScreen.js
+++ src/pages/homeScreen/HomeScreen.js
@@ -5,7 +5,6 @@ import GenericPage from "../../components/genericPage/GenericPage";
 export default function homeScreen() {
   return (
     <>
-      <GenericPage body="Home Screen" />
-    </>
+        <GenericPage body=<iframe title="report" width="1280" height="1080" src="https://datastudio.google.com/embed/reporting/f86e0f9c-502d-4b99-807b-88dec5dd10af/page/LuBV" frameBorder="0" /> /></>
   );
 }
